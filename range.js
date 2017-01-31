9a1fa0eee81d4e6f48e76dd00e59dbbcfd3089d4
Ronnel Bautista <ronnel.j.bautista@gmail.com>
2017-01-30 18:02:35 -0800

    Print Range Assignment

---
+++ range.js
@@ -0,0 +1,44 @@
+//Range Print
+//Create a function that can take a start point, end point, and skip amount, to print all numbers in the range.
+
+//Bonus(if there is time)
+//Make sure it works for negative numbers
+
+//If the user doesnt pass a skip amount make it defaut to 1 (print range(4,8); would print 4,5,6,7)
+
+//if the user doesnt pass an end point, make it start at 0, and end at the first (printrange(4); would print 0,1,2,3)
+
+
+
+// function printrange(startpoint, endpoint, skipammount){
+//
+//   for (var i = startpoint; i < endpoint; i+= skipammount){
+//   console.log(i)
+//   }
+// }
+// printrange(20,100,5)
+
+
+// function printrange(startpoint, endpoint, skipammount){
+//
+//   for (var i= startpoint; i < endpoint; i += skipammount){
+//     console.log(i)
+//   }
+// }
+// printrange(-10,0,2)
+
+// function printrange(startpoint, endpoint, skipammount){
+//
+//   for (var i = startpoint; i < endpoint; i += skipammount){
+//     console.log(i)
+//   }
+// }
+// printrange(4, 8,1)
+
+// function printrange(startpoint, endpoint, skipammount){
+//
+//   for (var i = startpoint; i < endpoint; i += skipammount){
+//     console.log(i)
+//   }
+// }
+// printrange(0,4,1)
